5e93546dcc755d0bc97b3b621f1f9dbc99f9010a
Frank Stekelenburg <frankstekelenburg@ziggo.nl>
2022-02-25 15:48:31 +0100

    tutorial episode 40
    verder bij 41

--- themes/demo/assets/dist/js/app.js
+++ themes/demo/assets/dist/js/app.js
@@ -3,12 +3,6 @@ var __webpack_exports__ = {};
 /*!**************************************!*\
   !*** ./themes/demo/assets/js/app.js ***!
   \**************************************/
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 /*
  * Application
  */
@@ -50,13 +44,12 @@ $(function () {
 //
 // sayHi('john');
 // sayBye('jeko');
-
-// var helloWorld = /*#__PURE__*/_createClass(function helloWorld() {
-//   _classCallCheck(this, helloWorld);
+// class helloWorld{
+//     constructor(){
+//         alert('hello world');
+//     }
+// }
 //
-//   alert('hello world');
-// });
-
-new helloWorld();
+// new helloWorld();
 /******/ })()
 ;
